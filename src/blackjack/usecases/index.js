b3a16b0aa13a70ebacc3405ae7f90ab05120ace5
Jesus Eduardo <devmr.qreb@outlook.com>
2023-07-24 13:08:29 -0600

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearCartaHTML } from './create-cardHTML';
+export { crearDeck } from './create-deck';
+export { pedirCarta } from './request-card';
+export { turnoComputadora } from './computer-turn';
+export { valorCarta } from './value-card';
